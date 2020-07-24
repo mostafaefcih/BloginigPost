7d286f7e6851d98d380282346e2666bb34880bf2
mostafaefcih <47594209+mostafaefcih@users.noreply.github.com>
2020-07-24 06:24:02 +0200

    Update ApplicationUser.cs
    remove add

--- SP_ASPNET_1/Models/ApplicationUser.cs
+++ SP_ASPNET_1/Models/ApplicationUser.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;add 
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
